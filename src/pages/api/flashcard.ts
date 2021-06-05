dd6f3e78c32575237dca75c41e26195aff3a764f
Pedro Arantes <arantespp@gmail.com>
2021-06-05 14:58:34 -0300

    fix: api

--- src/pages/api/flashcard.ts
+++ src/pages/api/flashcard.ts
@@ -1,10 +1,1 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-import { getFlashcard } from '../../lib/getFlashcard';
-
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const flashcard = await getFlashcard();
-  res.status(200).json({ flashcard });
-};
-
-export default handler;
+export { default } from '../../../api/flashcard';
