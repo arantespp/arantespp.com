1e3581f08760379e6281d22184462518bb12e05d
Pedro Arantes <arantespp@gmail.com>
2021-06-23 12:13:57 -0300

    chore: testing twitter universal tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -35,10 +35,24 @@ class MyDocument extends Document {
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());              
                   gtag('config', 'UA-149485554-1');
-                } else {
-                  console.log("Running on localhost, not calling gtag.")
                 }
               `,
+            }}
+          />
+          <script
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+              var host = window.location.hostname;
+              if(host != "localhost") {
+                !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+                },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
+                a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+                // Insert Twitter Pixel ID and Standard Event data below
+                twq('init','o69i6');
+                twq('track','PageView');
+              }
+          `,
             }}
           />
         </Head>
