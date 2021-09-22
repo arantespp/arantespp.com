1dfd706016f539cd14fa4daa51f46e966005dd87
Pedro Arantes <arantespp@gmail.com>
2021-09-22 15:04:09 -0300

    chore: system commit

--- next.config.js
+++ next.config.js
@@ -4,22 +4,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-/**
- * Some day, I need to remove this line and check if rehype-raw works without
- * next-transpile-modules. I removed it because it breaks the Twitter embed.
- */
-// const withTM = require('next-transpile-modules')([
-//   'rehype-raw',
-//   'hast-util-raw',
-//   'unist-util-position',
-//   'property-information',
-//   'web-namespaces',
-//   'hast-util-to-parse5',
-//   'hast-to-hyperscript',
-//   'zwitch',
-//   'html-void-elements',
-// ]);
-
 const { getRewrites, getRedirects } = require('./src/lib/links');
 
 module.exports = withBundleAnalyzer({
