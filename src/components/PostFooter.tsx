3735ff41c83f0132d91c832d28ec84efda6f95c4
Pedro Arantes <arantespp@gmail.com>
2022-03-07 12:34:02 -0300

    chore: system commit

--- src/components/PostFooter.tsx
+++ src/components/PostFooter.tsx
@@ -1,13 +1,33 @@
 import { Flex, Image } from 'theme-ui';
 
+const NO_FOOTER = true;
+
 export const PostFooter = () => {
+  if (NO_FOOTER) {
+    return (
+      <Flex
+        sx={{
+          marginTop: 5,
+          marginBottom: 5,
+        }}
+      />
+    );
+  }
+
   return (
-    <Flex sx={{ justifyContent: 'center', marginTop: 5, marginBottom: 6 }}>
+    <Flex
+      sx={{
+        justifyContent: 'center',
+        marginTop: 5,
+        marginBottom: 6,
+      }}
+    >
       <Image
         role="button"
         onClick={() => window.scroll({ top: 0, left: 0 })}
         sx={{ height: '1.5em', marginLeft: 1, cursor: 'pointer' }}
-        src="/rose.png"
+        src="/logo.png"
+        alt="Logo"
       />
     </Flex>
   );
