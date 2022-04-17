922ecb8fd768f03d52853c015a7c2201fbc3c269
Pedro Arantes <arantespp@gmail.com>
2022-04-16 23:00:40 -0300

    chore: system commit

--- lib/journal.ts
+++ lib/journal.ts
@@ -3,6 +3,7 @@ import {
   GetItemCommand,
   PutItemCommand,
   QueryCommand,
+  UpdateItemCommand,
 } from '@aws-sdk/client-dynamodb';
 import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
 import * as dateFns from 'date-fns';
@@ -19,7 +20,9 @@ const client = new DynamoDBClient({
 
 const TableName = process.env.AWS_DYNAMODB_TABLE as string;
 
-const pk = 'journal';
+const JOURNAL_PK = 'journal';
+
+const BACKUP_JOURNAL_PK = 'backup-journal';
 
 type JournalFromDatabase = { pk: string; sk: string; content: string };
 
@@ -45,7 +48,7 @@ export const getJournals = async (
     new QueryCommand({
       TableName,
       ExpressionAttributeValues: marshall({
-        ':pk': pk,
+        ':pk': JOURNAL_PK,
       }),
       KeyConditionExpression: 'pk = :pk',
       Limit: limit,
@@ -80,7 +83,7 @@ export const getJournal = async ({ date }: { date: string | Date }) => {
   const { Item } = await client.send(
     new GetItemCommand({
       TableName,
-      Key: marshall({ pk, sk: dateKey }),
+      Key: marshall({ pk: JOURNAL_PK, sk: dateKey }),
     }),
   );
 
@@ -91,23 +94,97 @@ export const getJournal = async ({ date }: { date: string | Date }) => {
   return mapJournal(unmarshall(Item) as JournalFromDatabase);
 };
 
-export const saveJournal = async ({
-  date,
-  content,
-}: {
+type SaveJournalArgs = {
   date: string;
   content: string;
-}) => {
+};
+
+const putJournal = async ({ date, content }: SaveJournalArgs) => {
   await client.send(
     new PutItemCommand({
       TableName,
       Item: marshall({
-        pk,
+        pk: JOURNAL_PK,
         sk: date,
         content,
       }),
     }),
   );
+};
+
+/**
+ * If someday I need it.
+ */
+// const getBackupJournal = async ({ date }: { date: string }) => {
+//   const { Item } = await client.send(
+//     new GetItemCommand({
+//       TableName,
+//       Key: marshall({ pk: BACKUP_JOURNAL_PK, sk: date }),
+//     }),
+//   );
+
+//   if (!Item) {
+//     return undefined;
+//   }
+
+//   return unmarshall(Item);
+// };
+
+const backupJournal = async ({ date, content }: SaveJournalArgs) => {
+  const now = dateFns.format(
+    dateFns.roundToNearestMinutes(new Date()),
+    'yyyy-MM-dd HH:mm',
+  );
+
+  try {
+    await client.send(
+      new UpdateItemCommand({
+        TableName,
+        Key: marshall({ pk: BACKUP_JOURNAL_PK, sk: date }),
+        UpdateExpression: 'SET #history.#now = :content',
+        ExpressionAttributeNames: {
+          '#history': 'history',
+          '#now': now,
+        },
+        ExpressionAttributeValues: marshall({
+          ':content': content,
+        }),
+      }),
+    );
+  } catch (err) {
+    if (
+      err.message ===
+      'The document path provided in the update expression is invalid for update'
+    ) {
+      await client.send(
+        new PutItemCommand({
+          TableName,
+          Item: marshall({
+            pk: BACKUP_JOURNAL_PK,
+            sk: date,
+            history: {
+              [now]: content,
+            },
+          }),
+        }),
+      );
+
+      return;
+    }
+
+    console.error(err.message);
+  }
+};
+
+export const saveJournal = async ({ date, content }: SaveJournalArgs) => {
+  if (!content) {
+    return;
+  }
+
+  await Promise.all([
+    putJournal({ date, content }),
+    backupJournal({ date, content }),
+  ]);
 };
 
 export const getJournalsSummary = async ({ date }: { date: string | Date }) => {
@@ -141,3 +218,13 @@ type ThenArg<T> = T extends PromiseLike<infer U> ? U : T;
 export type JournalsSummary = NonNullable<
   ThenArg<ReturnType<typeof getJournalsSummary>>
 >;
+
+// Levantei 6h15 e comecei e voltei a ler Principles, a parte que falava de ser mente aberta. Em uma das passagens, o livro fala de você usar a dor mental como um sinal de que você não está sendo mente aberta. Quando alguém fala algo oposto da sua visão, você pode sentir uma dor porque o seu ego foi atacado. Quando isso ocorre, pode ser porque você está errado e a outra pessoa está certa. A vantagem de meditar é conseguir perceber estas dores ou quando o seu eu baixo nível (emoções) toma conta. Eu lembro que aconteceu isso comigo quando o Ennio falou do botão do Siflor.
+
+// No dia que ele falou que eu deixei passar o botão, eu senti o ego ferido, mas eu consegui perceber isso na hora. No fundo eu estava errado.
+
+// **A pior combinação de uma pessoa é quando ela é mente fechada e tem muitos pontos cegos.**
+
+// ---
+
+// Tive um insight do que são os 4 corpos na minha meditação hoje. Físico é o corpo. Emocional é a parte primitiva do cérebro. Mental é o neocortex. Espiritual é talvez alguma parte do cérebro junto com a Inteligência Universal.
