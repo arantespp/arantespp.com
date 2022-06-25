34c4e04b1b238c378ebf3e3eaa2eb87575b48248
Pedro Arantes <arantespp@gmail.com>
2022-06-25 20:44:23 -0300

    chore: system commit

--- src/pages/newsletter.tsx
+++ src/pages/newsletter.tsx
@@ -1,38 +1,36 @@
 import * as React from 'react';
-import { Button, Flex, Input, Themed } from 'theme-ui';
-import { useQuery } from 'react-query';
-
-import Flashcard from '../components/Flashcard';
+// import { Button, Flex, Input, Themed } from 'theme-ui';
+// import { NewsletterData } from '../../lib/newsletter';
+// import { useQuery } from 'react-query';
+// import Flashcard from '../components/Flashcard';
 import Heading from '../components/Heading';
-import Link from '../components/Link';
-import Loading from '../components/Loading';
+// import Link from '../components/Link';
+// import Loading from '../components/Loading';
 
-import type { NewsletterData } from '../../lib/newsletter';
-
-const saveNewsletterItems = async (item: any) => {
-  return fetch('/api/newsletter/items', {
-    method: 'PUT',
-    body: JSON.stringify(item),
-  });
-};
+// const saveNewsletterItems = async (item: any) => {
+//   return fetch('/api/newsletter/items', {
+//     method: 'PUT',
+//     body: JSON.stringify(item),
+//   });
+// };
 
 const Newsletter = () => {
-  const { data } = useQuery<NewsletterData>(`/api/newsletter`, async () =>
-    fetch(`/api/newsletter`).then((r) => r.json()),
-  );
+  // const { data } = useQuery<NewsletterData>(`/api/newsletter`, async () =>
+  //   fetch(`/api/newsletter`).then((r) => r.json()),
+  // );
 
-  const readingInputRef = React.useRef<HTMLInputElement>(null);
+  // const readingInputRef = React.useRef<HTMLInputElement>(null);
 
-  if (!data) {
-    return <Loading />;
-  }
+  // if (!data) {
+  //   return <Loading />;
+  // }
 
-  const { flashcard, twitterSearchUrl } = data;
+  // const { flashcard, twitterSearchUrl } = data;
 
   return (
     <>
       <Heading level={1}>Newsletter</Heading>
-      <Themed.p>
+      {/* <Themed.p>
         This page is my tool to create my newsletter following the format
         proposed in <Link href="/a/newsletter-v1">this article.</Link>
       </Themed.p>
@@ -57,7 +55,7 @@ const Newsletter = () => {
         Choose a tweet from this search to be featured on the newsletter.
       </Link>
       <Heading level={3}>Spacing Effect</Heading>
-      <Flashcard flashcard={flashcard} />
+      <Flashcard flashcard={flashcard} /> */}
     </>
   );
 };
